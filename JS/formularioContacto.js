43ce529aec326819a9e515f642194b09644a83a9
Sharon <sharon.ormachea@gmail.com>
2022-10-26 20:12:32 -0300

    Medias Querys del formulario

--- JS/formularioContacto.js
+++ JS/formularioContacto.js
@@ -1,5 +1,9 @@
 const formulario = document.getElementById("formulario");
-const inputs = document.querySelectorAll("#formulario input");
+const inputNombre = document.getElementById("nombre");
+const inputApellido = document.getElementById("apellido");
+const inputEmail = document.getElementById("email");
+const inputTelefono = document.getElementById("telefono");
+const inputs = [];
 
 // Expresiones regulares
 // "/^" indica donde empieza y "$/" indica donde termina
@@ -16,38 +20,55 @@ const expresiones = {
     telObligatorio: /^\d{7,14}$/ // Toma de 7 a 14 numeros
 }
 
+const campos = {
+    nomObligatorio: false,
+    apObligatorio: false,
+    emailObligatorio: false,
+    telObligatorio: false
+}
+
 const validarFormulario = (event) => {
     switch (event.target.name) {
         case "nombre":
             validarCampo(expresiones.nomObligatorio, event.target, "nombre")
         break;
         case "apellido":
-            
+            validarCampo(expresiones.apObligatorio, event.target, "apellido")
         break;
         case "email":
-            
+            validarCampo(expresiones.emailObligatorio, event.target, "email")
         break;
         case "telefono":
-            
+            validarCampo(expresiones.telObligatorio, event.target, "telefono")
         break;
     }
 }
 
 const validarCampo = (expresion, input, campo) => {
     if(expresion.nomObligatorio.test(input.value)){
-        document.getElementById(`campo-${campo}`).classList.remove("formulario__grupo-incorrecto");
-        document.getElementById(`campo-${campo}`).classList.add("formulario__grupo-correcto");
-        document.querySelector(`#campo-${campo} i`).classList.add("fa-check-circle");
-        document.querySelector(`#campo-${campo} i`).classList.remove("fa-times-circle");
+        document.getElementById(`campo-nombre`).classList.remove("formulario__grupo-incorrecto");
+        document.getElementById(`campo-nombre`).classList.add("formulario__grupo-correcto");
+        document.getElementById(`campo-apellido`).classList.remove("formulario__grupo-incorrecto");
+        document.getElementById(`campo-apellido`).classList.add("formulario__grupo-correcto");
+        document.getElementById(`campo-email`).classList.remove("formulario__grupo-incorrecto");
+        document.getElementById(`campo-email`).classList.add("formulario__grupo-correcto");
+        document.getElementById(`campo-telefono`).classList.remove("formulario__grupo-incorrecto");
+        document.getElementById(`campo-telefono`).classList.add("formulario__grupo-correcto");
+        campos[campo] = true;
     } else{
-        document.getElementById(`campo-${campo}`).classList.add("formulario__grupo-incorrecto");
-        document.getElementById(`campo-${campo}`).classList.remove("formulario__grupo-correcto");
-        document.querySelector(`#campo-${campo} i`).classList.add("fa-times-circle");
-        document.querySelector(`#campo-${campo} i`).classList.remove("fa-check-circle");
+        document.getElementById(`campo-nombre`).classList.add("formulario__grupo-incorrecto");
+        document.getElementById(`campo-nombre`).classList.remove("formulario__grupo-correcto");
+        document.getElementById(`campo-apellido`).classList.add("formulario__grupo-incorrecto");
+        document.getElementById(`campo-apellido`).classList.remove("formulario__grupo-correcto");
+        document.getElementById(`campo-email`).classList.add("formulario__grupo-incorrecto");
+        document.getElementById(`campo-email`).classList.remove("formulario__grupo-correcto");
+        document.getElementById(`campo-telefono`).classList.add("formulario__grupo-incorrecto");
+        document.getElementById(`campo-telefono`).classList.remove("formulario__grupo-correcto");
+        campos[campo] = false;
     }
 }
 
-inputs.forEach( (input) => {
+inputs.forEach((input) => {
     input.addEventListener("keyup", validarFormulario);
     input.addEventListener("blur", validarFormulario);
 });
@@ -56,3 +77,25 @@ inputs.forEach( (input) => {
 formulario.addEventListener("submit", (evento) => {
     evento.preventDefault();
 });
+
+
+// Codigo para consumir la API del formulario
+
+// const $form = document.querySelector("#formulario")
+
+// $form.addEventListener("submit", handleSubmit)
+
+// async function handleSubmit(event){
+//     event.preventDefault()
+//     const form = new FormData(this)
+//     const response = await fetch(this.action, {
+//         method: this.method,
+//         body: form,
+//         headers: {
+//             "Accept": "application/json"
+//         }
+//     })
+//     if(response.ok){
+//         alert("Gracias por enviarnos su consulta. En breve nos pondremos en contacto con usted")
+//     }
+// }
